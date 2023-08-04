1fe9092bff9a0820d704add9c3538e6bcecc29da
Charles <uchecharles223@gmail.com>
2023-08-04 02:36:12 +0100

    seting the styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import React from 'react';
+import {
+  // MDBBtn,
+  MDBContainer,
+  MDBCard,
+  // MDBCardBody,
+  // MDBCardImage,
+  MDBRow,
+  MDBCol,
+  // MDBIcon,
+  // MDBInput
+}
+from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section class="">
+
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-lg-6 vh-100 bg-danger">
+      </div>
+      <div className="col-lg-6 vh-100 bg-primary">
+      </div>
     </div>
+  </div>
+
+</section>
   );
 }
 
 export default App;
+
+
